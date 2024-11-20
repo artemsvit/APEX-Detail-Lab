ba0ed4d30beabf37c35415d746e6312881f003b2
artemsvit <156072729+artemsvit@users.noreply.github.com>
2024-11-20 14:39:09 +0000

    Deploying to gh-pages from @ artemsvit/APEX-Detail-Lab@e0de35e214df783be67a35a288f964c9bbc865ae 🚀

---
+++ _next/static/chunks/app/works/[id]/not-found-160019113b3473cf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[265,617],{59468:function(n,e,u){Promise.resolve().then(u.t.bind(u,68326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=59468)}),_N_E=n.O()}]);
--- _next/static/chunks/app/works/[id]/not-found-f77a2c6101baadbf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[265,617],{9468:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=9468)}),_N_E=n.O()}]);
